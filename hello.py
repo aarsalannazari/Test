6b7f2760fe0e20f82649c2035f195bba76af9cc1
aarsalannazari <119811476+aarsalannazari@users.noreply.github.com>
2023-01-09 01:28:36 +0330

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,5 +1,12 @@
 from tkinter import *
 from tkinter import messagebox
+def text_message():
+	text.set('''hello world
+I am Amirarsalan
+I am 12 years old
+I am almost beginner and I love programming
+I live in Iran and I am originally Iranian''')
+	messagebox.showinfo('infotmation',text.get())
 window=Tk()
 window.geometry('150x100')
 window.configure(background='gray')
@@ -8,9 +15,5 @@ window.resizable(width=False,height=False)
 text=StringVar()
 label=Label(window,textvariable=text).pack(side=TOP)
 
-button=Button(window,text='test',bd=5,command=lambda:text.set('''hello world
-I am Amirarsalan
-I am 12 years old
-I am almost beginner and I love programming
-I live in Iran and I am originally Iranian''')).pack()
+button=Button(window,text='test',bd=5,command=lambda:text_message()).pack()
 window.mainloop()
