91c3159262a2547f247037e0ed08bd36df0464e0
aarsalannazari <119811476+aarsalannazari@users.noreply.github.com>
2023-01-09 01:42:39 +0330

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,12 +1,19 @@
 from tkinter import *
 from tkinter import messagebox
-def text_message():
+def text_message_e():
 	text.set('''hello world
 I am Amirarsalan
 I am 12 years old
 I am almost beginner and I love programming
 I live in Iran and I am originally Iranian''')
 	messagebox.showinfo('infotmation',text.get())
+def text_message_f():
+	text.set('''سلام مردم دنیا
+من امیر ارسلان هستم
+من دوازده سال دارم
+من تقریبا مبتدی هستم اما دانشم از مبتدی یکم بالا تره
+من تو ایران زندگی میکنم و اصالتا هم ایرانی هستم''')
+	messagebox.showinfo('اطلاعات',text.get())
 window=Tk()
 window.geometry('150x100')
 window.configure(background='gray')
@@ -15,5 +22,6 @@ window.resizable(width=False,height=False)
 text=StringVar()
 label=Label(window,textvariable=text).pack(side=TOP)
 
-button=Button(window,text='test',bd=5,command=lambda:text_message()).pack()
+button=Button(window,text='test',bd=5,command=lambda:text_message_e()).pack()
+button=Button(window,text='تست',bd=5,command=lambda:text_message_f()).pack()
 window.mainloop()
